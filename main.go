9a014cdb574835b752bff18ff00c94fa927a3d29
mtttech <mtaylor9754@hotmail.com>
2024-03-30 22:13:19 -0400

    Format fix.

--- main.go
+++ main.go
@@ -23,27 +23,27 @@ func (v *Vampire) GetMyClan() string {
 }
 
 func Delete(arr []string, needle string) []string {
-  var index_begin, index_end int
-  for index, value := range arr {
-    if value == needle {
-      index_begin = index
-      index_end = index + 1
-    }
-  }
-  // Nothing was found, return original array
-  if index_begin == 0 && index_end == 0 {
-    return arr
-  }
-  // Return list without needle
-  return append(arr[:index_begin], arr[index_end:]...)
+	var index_begin, index_end int
+	for index, value := range arr {
+    	if value == needle {
+			index_begin = index
+			index_end = index + 1
+    	}
+	}
+  	// Nothing was found, return original array
+  	if index_begin == 0 && index_end == 0 {
+		return arr
+	}
+	// Return list without needle
+	return append(arr[:index_begin], arr[index_end:]...)
 }
 
 func main() {
-  t := []string{"A", "One", "B", "Two", "C"}
-  fmt.Println(Delete(t, "A"))
+	t := []string{"A", "One", "B", "Two", "C"}
+	fmt.Println(Delete(t, "A"))
 
 	attributes := []string{"Charisma", "Dexterity", "Composure", "Intelligence", "Manipulation", "Resolve", "Stamina", "Strength", "Wits"}
-  //attribute_dots := []int{4, 3, 3, 3, 2, 2, 2, 2, 1}
+	//attribute_dots := []int{4, 3, 3, 3, 2, 2, 2, 2, 1}
 	genders := []string{"Female", "Male"}
 	clans := []string{"Brujah", "Gangrel", "Malkavian", "Nosferatu", "Thin-Blood", "Toreador", "Tremere", "Ventrue"}
 
@@ -62,9 +62,9 @@ func main() {
 		fmt.Println("You selected the clan", clan)
 	}
 
-  for i := range 9 {
-    fmt.Println(attributes[i])
-  }
+	for i := range 9 {
+    	fmt.Println(attributes[i])
+	}
 
 	v := Vampire{name: name, gender: gender, clan: clan}
 	fmt.Printf("%s %s %s.\n", v.GetMyName(), v.GetMyGender(), v.GetMyClan())
